13e42843c941cd7f774e0ba0bdb4845f4b1298b1
Jongseok Choi <cjongseok@gmail.com>
2018-03-22 18:39:56 +0900

    Add scripts and assets for Docker image

---
+++ mtprotod/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# build a linux executable
+CGO_ENABLED=0 GOOS=linux go build -ldflags '-s' -a -installsuffix cgo -o main
+
+# build & push a Docker image
+proxyVersion=$(go run main.go --version | awk '{print $3}')
+dockerImage=cjongseok/mtprotod:${proxyVersion}
+latestImage=cjongseok/mtprotod:latest
+docker build -t ${dockerImage} .
+docker tag ${dockerImage} ${latestImage}
+docker login
+docker push ${dockerImage}
+docker push ${latestImage}
