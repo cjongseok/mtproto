4bb0f522292c8628496cf20c50e760749aa1aae7
cjongseok <cjongseok@gmail.com>
2018-06-03 21:58:22 +0900

    Update build_py.sh

--- compiler/build_py.sh
+++ compiler/build_py.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+python3 -m pip install grpcio-tools
 cp ../types.tl.proto ../tl.proto
 cp ../proxy/tl_update.proto ../proxy/update.proto
 python3 -m  grpc_tools.protoc  -I .. -I ~/Programs/protoc-3.5.1/include --python_out=../py --grpc_python_out=../py ../tl.proto
